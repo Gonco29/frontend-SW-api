9f02f71fe3a62815a38e134ee65cf387ef8fd0b0
Gonzalo Cobos Daract <goncobos@gmail.com>
2022-12-23 20:34:06 -0300

    coding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    result = RestClient.get("http://localhost:3001/api/v1/characters")
+    @characters = JSON.parse(result)
   end
 
 end
